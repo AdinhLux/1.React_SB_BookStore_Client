23405d829915e1bb6d42e952f0f379e8409741b5
ADI <adinh@jiway.lu>
2021-09-25 14:08:16 +0200

    App.js : export 'App' component to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// Creating a dummy component
-const App = () => {
-  return <div>My first component</div>;
-}
+import App from "./App";
 
 // Render component with ReactDOM to the index.html (in 'public' folder) in '<div id="root">'
 ReactDOM.render(<App />, document.getElementById('root'));
