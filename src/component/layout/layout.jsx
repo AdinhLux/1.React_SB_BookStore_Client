01acefd6eef485e648738440387f797da10feb23
ADI <adinh@jiway.lu>
2021-09-28 21:35:35 +0200

    NEW src/component/layout/Header.jsx + implementation

--- src/component/layout/layout.jsx
+++ src/component/layout/layout.jsx
@@ -1,6 +1,7 @@
 import { Box } from "@material-ui/core";
 import React from "react";
 import PropTypes from "prop-types";
+import Header from "./Header";
 
 // PropTypes exports a range of validators that can be used to make sure the data you receive is valid.
 // If someone wants to use this layout, he needs to provide value to children property
@@ -14,7 +15,7 @@ const Layout = ({ children }) => {
       <Box>
         <Header />
       </Box>
-      // mt = margin top, ml = margin left
+      (mt = margin top, ml = margin left)
       <Box mt={8} ml={5}>
         {children}
       </Box>
