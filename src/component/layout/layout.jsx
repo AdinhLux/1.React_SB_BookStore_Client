45d6504e50f31c217642b5bfff58f1e61e997ec0
ADI <adinh@jiway.lu>
2021-09-28 21:25:36 +0200

    layout.jsx : implement prop-types

--- src/component/layout/layout.jsx
+++ src/component/layout/layout.jsx
@@ -1,11 +1,18 @@
 import { Box } from "@material-ui/core";
 import React from "react";
+import PropTypes from "prop-types";
+
+// PropTypes exports a range of validators that can be used to make sure the data you receive is valid.
+// If someone wants to use this layout, he needs to provide value to children property
+const propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 const Layout = ({ children }) => {
   return (
     <Box>
       <Box>
-        <Header></Header>
+        <Header />
       </Box>
       // mt = margin top, ml = margin left
       <Box mt={8} ml={5}>
@@ -15,4 +22,5 @@ const Layout = ({ children }) => {
   );
 };
 
+Layout.propTypes = propTypes;
 export default Layout;
