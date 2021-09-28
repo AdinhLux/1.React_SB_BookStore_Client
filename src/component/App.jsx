04ea9e41d1d6833c2351ccd51b01b6b83c69b400
ADI <adinh@jiway.lu>
2021-09-28 21:36:02 +0200

    src/component/App.jsx : implement Layout.jsx

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,18 +1,24 @@
 // We need to import React to make this const a component
-import React from 'react';
+import React from "react";
 
-import axios from 'axios';
-import baseUrl from '../config'
+import axios from "axios";
+import baseUrl from "../config";
+
+import Layout from "./layout/Layout";
 
 // Creating a dummy component
 const App = () => {
-    // Endpoint of Book to get all books
-    axios(`${baseUrl}/api/v1/books`).then(books => {
-        console.log(books);
-    });
+  // Endpoint of Book to get all books
+  axios(`${baseUrl}/api/v1/books`).then((books) => {
+    console.log(books);
+  });
 
-    return <div>My first component</div>;
-}
+  return (
+    <Layout>
+      <div>My first component</div>
+    </Layout>
+  );
+};
 
 // Export to outside as 'App'
 export default App;
