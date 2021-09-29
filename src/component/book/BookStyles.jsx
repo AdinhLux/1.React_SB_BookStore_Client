38702c37a74ba5fb151cde49a519d7bd3f722b5b
ADI <adinh@jiway.lu>
2021-09-29 20:26:22 +0200

    NEW src/component/book/BookStyles.jsx : where we define CSS property for Book components

---
+++ src/component/book/BookStyles.jsx
@@ -0,0 +1,26 @@
+// Import Look and Feel : recommended way of using CSS
+import { makeStyles, createStyles } from "@material-ui/core";
+
+export default makeStyles(() =>
+  createStyles({
+    bookContainer: {
+      display: "flex",
+      justifyContent: "center",
+      flexDirection: "row",
+    },
+
+    bookFilter: {
+      width: "20%",
+      height: "500px",
+    },
+
+    bookFilterPaper: {
+      width: "100%",
+      height: "100%",
+    },
+
+    bookList: {
+      width: "80%",
+    },
+  })
+);
