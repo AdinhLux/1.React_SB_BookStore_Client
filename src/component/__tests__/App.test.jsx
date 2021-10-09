206323bdd2cefff1c94f7ed02d6ebb34bcfcdca1
ADI <adinh@jiway.lu>
2021-10-09 21:34:37 +0200

    App.test.jsx : adapt code based on index.jsx + DELETE SNAPSHOT TO CREATE A NEW ONE

--- src/component/__tests__/App.test.jsx
+++ src/component/__tests__/App.test.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { render } from "@testing-library/react";
 
+import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "../../module";
+
 // Import your component for testing
 import App from "../App";
 
 describe("App component", () => {
   it("should render app with error", () => {
+    const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+
     // asFragment' is a method that returns a DocumentFragment of our rendered component.
-    const { asFragment } = render(<App />);
+    const { asFragment } = render(
+      <Provider
+        store={createStoreWithMiddleware(
+          reducers,
+          window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        )}
+      >
+        <App />
+      </Provider>
+    );
 
     // Testing with Jest snapshot
     // Snapshots are generally testing the initial rendering of a component
