95a7d046d138eaa649ad9591b716a1e0bd4f4472
ADI <adinh@jiway.lu>
2021-10-10 19:51:26 +0200

    NEW bookReducer.test.jsx : FAILURE test with npm run test -- <File_Path>

---
+++ src/module/book/__tests__/bookReducer.test.jsx
@@ -0,0 +1,29 @@
+import bookReducer, { INITIAL_BOOK_REDUCER_STATE } from "../bookReducer";
+
+describe("bookReducer test", () => {
+  it("should return the right state", () => {
+    const action = {
+      type: "BOOKLIST",
+      payload: [
+        {
+          id: 1,
+          title: "test title",
+          description: "test description",
+          releaseYear: 2018,
+        },
+      ],
+    };
+
+    const newState = bookReducer(INITIAL_BOOK_REDUCER_STATE, action);
+
+    // Asserting new state
+    expect(newState).toEqual([
+      {
+        id: 1,
+        title: "test title",
+        description: "test description",
+        releaseYear: 2018,
+      },
+    ]);
+  });
+});
