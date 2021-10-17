a47f1fa9303084927679243325c7ce09edce0eb0
ADI <adinh@jiway.lu>
2021-10-17 22:22:03 +0200

    BookContainer.jsx : implementing Loader (Skeleton)

--- src/component/book/BookContainer.jsx
+++ src/component/book/BookContainer.jsx
@@ -2,10 +2,14 @@ import { Box } from "@material-ui/core";
 import { useDispatch, useSelector } from "react-redux";
 import getBooksAction from "../../module/book/bookAction";
 import React, { useEffect } from "react";
-import { getBooksSelector } from "../../module/book/bookSelector";
+import {
+  getBooksSelector,
+  getBookPromiseSelector,
+} from "../../module/book/bookSelector";
 import BookFilter from "./BookFilter";
 import styles from "./BookStyles";
 import BookList from "./BookList";
+import { Skeleton } from "@material-ui/lab";
 
 const BookContainer = () => {
   /**
@@ -35,6 +39,8 @@ const BookContainer = () => {
    * So, Book container is rendered with new data.
    */
   const books = useSelector(getBooksSelector);
+  const bookPromise = useSelector(getBookPromiseSelector);
+
   console.log("Book Container useSelector books: ", books);
 
   // Instantiate BookStyles
@@ -43,7 +49,30 @@ const BookContainer = () => {
     <Box className={classes.bookContainer}>
       <BookFilter />
       <Box className={classes.bookList}>
-        <BookList books={books} />
+        {
+          // Le's display Loader
+          bookPromise.isPending && (
+            <Box ml={5}>
+              <Skeleton
+                data-testid="book-loader"
+                variant="rect"
+                animation="pulse"
+                width="80%"
+                height={200}
+              />
+            </Box>
+          )
+        }
+        {
+          // Display error message
+          bookPromise.isErrorOccured && (
+            <div data-testid="book-error-message"> Error message ... </div>
+          )
+        }
+        {
+          // Display Data
+          bookPromise.isFulfilled && <BookList books={books} />
+        }
       </Box>
     </Box>
   );
