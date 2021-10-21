9efc678488130d3676698ab043deb5bb0a45fd95
ADI <adinh@jiway.lu>
2021-10-21 21:10:08 +0200

    NEW BookFilter.test.jsx : simulate Search Button & TextField

--- src/component/book/BookFilter.jsx
+++ src/component/book/BookFilter.jsx
@@ -1,14 +1,39 @@
-import { Paper, Box } from "@material-ui/core";
-import React from "react";
+import { Paper, Box, Typography, TextField, Button } from "@material-ui/core";
+import React, { useState } from "react";
 import styles from "./BookStyles";
+import { getBooksByTitle } from "../../module/book/bookAction";
 
 const BookFilter = () => {
   // Instantiate BookStyles
   const classes = styles();
 
+  // Defining some internal states
+  const [searchText, setSearchText] = useState("");
+
   return (
     <Box className={classes.bookFilter}>
-      <Paper className={classes.bookFilterPaper}>Book filter</Paper>
+      <Paper className={classes.bookFilterPaper}>
+        <Typography>Search Books Filter</Typography>
+        <Box>
+          <TextField
+            placeholder="Enter Book title"
+            id="book-search"
+            data-testid="book-title-input"
+            label="Enter book title"
+            variant="outlined"
+            value={searchText}
+            onClick={(event) => setSearchText(event.target.value)}
+          />
+        </Box>
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => getBooksByTitle(searchText)}
+        >
+          Search
+        </Button>
+      </Paper>
     </Box>
   );
 };
---
+++ src/component/book/__tests__/BookFilter.test.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import BookFilter from "../BookFilter";
+import renderWithRedux from "../../../util/testUtil";
+import { fireEvent } from "@testing-library/dom";
+import { getBooksByTitle } from "../../../module/book/bookAction";
+
+// Mocking methods
+jest.mock("../../../module/book/bookAction", () => ({
+  getBooksByTitle: jest.fn(),
+}));
+
+describe("BookFilter", () => {
+  it("should fire getBooksByTitle action on search button click", () => {
+    const { getByLabelText, getByText } = renderWithRedux(<BookFilter />, {});
+
+    const textField = getByLabelText("Enter book title");
+
+    // Simulate TextField reading
+    fireEvent.change(textField, { target: { value: "test title" } });
+
+    const searchButton = getByText("Search");
+
+    // Simulate bouton click
+    fireEvent.click(searchButton);
+
+    expect(getBooksByTitle).toHaveBeenCalledWith("test title");
+  });
+});
