b3cde15549b4bdfc88292cb919baf4b00c68b4eb
ADI <adinh@jiway.lu>
2021-10-21 19:01:21 +0200

    Reducer : BOOKSBYTITLE

--- src/module/book/__tests__/bookReducer.test.jsx
+++ src/module/book/__tests__/bookReducer.test.jsx
@@ -1,7 +1,7 @@
 import bookReducer, { INITIAL_BOOK_REDUCER_STATE } from "../bookReducer";
 
 describe("bookReducer test", () => {
-  it("should return the right state", () => {
+  it("should return the right new state", () => {
     const action = {
       type: "BOOKLIST",
       payload: [
@@ -22,6 +22,40 @@ describe("bookReducer test", () => {
         {
           id: 1,
           title: "test title",
+          description: "test description",
+          releaseYear: 2018,
+        },
+      ],
+
+      promise: {
+        isPending: false,
+        isFulfilled: false,
+        isErrorOccured: false,
+      },
+    });
+  });
+
+  it("should return the right new state for BOOKSBYTITLE Action", () => {
+    const action = {
+      type: "BOOKSBYTITLE",
+      payload: [
+        {
+          id: 1,
+          title: "second book",
+          description: "test description",
+          releaseYear: 2018,
+        },
+      ],
+    };
+
+    const newState = bookReducer(INITIAL_BOOK_REDUCER_STATE, action);
+
+    // Asserting new state
+    expect(newState).toEqual({
+      books: [
+        {
+          id: 1,
+          title: "second book",
           description: "test description",
           releaseYear: 2018,
         },
