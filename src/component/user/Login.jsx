1bddd8a46ee7bde0fe197cdb231dcdeb34fe1bc2
ADI <adinh@jiway.lu>
2021-10-30 17:05:23 +0200

    Login Page UI

--- src/component/user/Login.jsx
+++ src/component/user/Login.jsx
@@ -1,8 +1,45 @@
 import React from "react";
-import { Box } from "@material-ui/core";
+import { Box, Button, Paper, TextField, Typography } from "@material-ui/core";
+import makeStyle from "./LoginStyle";
 
 const Login = () => {
-  return <Box>login page</Box>;
+  const classes = makeStyle();
+
+  return (
+    <form autoComplete="off" noValidate>
+      <Box className={classes.wrapper}>
+        <Paper className={classes.paper}>
+          <Typography variant="h4">Book store Login</Typography>
+          <TextField
+            className={classes.topMargin}
+            name="email"
+            id="email"
+            data-testid="email-testid"
+            label="Enter email address"
+            variant="outlined"
+            placeholder="Enter email address"
+          />
+          <TextField
+            className={classes.topMargin}
+            name="password"
+            id="password"
+            data-testid="password-testid"
+            label="Enter password"
+            variant="outlined"
+            placeholder="Enter password"
+          />
+          <Button
+            className={classes.topMargin}
+            type="submit"
+            variant="contained"
+            color=" primary"
+          >
+            Login
+          </Button>
+        </Paper>
+      </Box>
+    </form>
+  );
 };
 
 export default Login;
