d68720f3a040700e7ce46fc8ffb6fb9412e93a8c
ADI <adinh@jiway.lu>
2021-10-30 20:41:10 +0200

    Login.jsx : implement form handling

--- src/component/user/Login.jsx
+++ src/component/user/Login.jsx
@@ -1,12 +1,43 @@
 import React from "react";
 import { Box, Button, Paper, TextField, Typography } from "@material-ui/core";
 import makeStyle from "./LoginStyle";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { useDispatch } from "react-redux";
+import { loginAction } from "../../module/user/userAction";
+
+// Yup is a JavaScript schema builder for value parsing and validation
+const validationSchema = yup.object({
+  email: yup
+    .string("Enter your email")
+    .email("Enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string("Enter your password")
+    .min(8, "Password should be of minimunm 8 characters length")
+    .required("Password is required"),
+});
 
 const Login = () => {
   const classes = makeStyle();
 
+  const dispatch = useDispatch();
+
+  // Formik helps us to build forms in React
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      // Reading values from form fields and attempting Login
+      dispatch(loginAction(values.email, values.password));
+    },
+  });
+
   return (
-    <form autoComplete="off" noValidate>
+    <form autoComplete="off" noValidate onSubmit={formik.handleSubmit}>
       <Box className={classes.wrapper}>
         <Paper className={classes.paper}>
           <Typography variant="h4">Book store Login</Typography>
@@ -18,6 +49,10 @@ const Login = () => {
             label="Enter email address"
             variant="outlined"
             placeholder="Enter email address"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            helperText={formik.touched.email && formik.errors.email}
+            error={formik.touched.email && Boolean(formik.errors.email)}
           />
           <TextField
             className={classes.topMargin}
@@ -27,6 +62,10 @@ const Login = () => {
             label="Enter password"
             variant="outlined"
             placeholder="Enter password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            helperText={formik.touched.password && formik.errors.password}
+            error={formik.touched.password && Boolean(formik.errors.password)}
           />
           <Button
             className={classes.topMargin}
